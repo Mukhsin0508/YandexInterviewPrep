18e93b4fdc59aef2d5c16a69641245816e8b2de4
Mukhsin0508 <mukhsinmukhtorov@arizona.edu>
2024-09-13 11:34:47 +0500

    third  push

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,14 +1,14 @@
-# # CaesarCipher
-#
-# def caesarCipher(text, shift):
-#     result = ""
-#     for i in range(len(text)):
-#         char = text[i]
-#         if char.isupper():
-#             result += chr((ord(char) + shift - 65) % 28 + 65) # 65 is the ASCII value of 'A'
-#         elif char.islower():
-#             result += chr((ord(char) + shift - 97) % 28 +97) # 97 is the ASCII value of 'a'
-#         else:
-#             result += char
-#     print(result)
-# caesarCipher(input("Enter text: "), int(input("Enter shift: ")))
+# CaesarCipher
+
+def caesarCipher(text, shift):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        if char.isupper():
+            result += chr((ord(char) + shift - 65) % 28 + 65) # 65 is the ASCII value of 'A'
+        elif char.islower():
+            result += chr((ord(char) + shift - 97) % 28 +97) # 97 is the ASCII value of 'a'
+        else:
+            result += char
+    print(result)
+caesarCipher(input("Enter text: "), int(input("Enter shift: ")))
